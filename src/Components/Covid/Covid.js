7ba859daab39b72fc9ef474eb81337b01793debb
Ali Mian <ali.najam1999@gmail.com>
2020-10-22 11:16:58 +0500

    changes to home.js

---
+++ src/Components/Covid/Covid.js
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import './Covid.css';
+import {withRouter} from "react-router"
+
+class Covid extends component{
+
+}
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,9 +7,21 @@ class Home extends Component {
 render(){
     return(
         <div className = "home">
-            
+            <div className = "title">
+            <h1>Search Covid-19 data by Country</h1>
         </div>
-    );
+
+        <div className = "Search">
+        <input type = "text" className = "searchBar"
+          placeholder= "Search..."
+          ref = {this.inputRef}
+          />
+
+        </div>
+
+        </div>
+
+    )
 }
 }
 export default Home;
